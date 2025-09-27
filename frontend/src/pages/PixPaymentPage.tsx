72ca5658a0dbfb452d3239d49828874ad56f776c
lmgaspa <luhmgasparetto@gmail.com>
2025-09-27 18:10:53 -0300

    herokucredit

--- frontend/src/pages/PixPaymentPage.tsx
+++ frontend/src/pages/PixPaymentPage.tsx
@@ -6,7 +6,7 @@ import { calcularFreteComBaseEmCarrinho } from "../utils/freteUtils";
 
 const API_BASE =
   import.meta.env.VITE_API_BASE ??
-  "https://ecommerceag-6fa0e6a5edbf.herokuapp.comm";
+  "https://ecommerceag-6fa0e6a5edbf.herokuapp.com";
 
 function formatMMSS(totalSec: number) {
   const m = Math.floor(totalSec / 60);
