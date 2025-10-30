a6b91d5f1da8a215772446f10ddfc64c007cb5b7
lmgaspa <luhmgasparetto@gmail.com>
2025-10-30 19:10:09 -0300

    cookies2

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,13 @@
-// src/main.tsx
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import { CartProvider } from "./context/CartContext";
+    // src/main.tsx
+    import { StrictMode } from "react";
+    import { createRoot } from "react-dom/client";
+    import App from "./App";
+    import { CartProvider } from "./context/CartContext";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <CartProvider>
-      <App />
-    </CartProvider>
-  </StrictMode>
-);
+    createRoot(document.getElementById("root")!).render(
+      <StrictMode>
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </StrictMode>
+    );
