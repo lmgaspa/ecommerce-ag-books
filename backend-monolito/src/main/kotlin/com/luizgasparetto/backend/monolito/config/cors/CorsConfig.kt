77e9eafd3f2f4ed245f553a4a892cdeb1bdc4816
lmgaspa <luhmgasparetto@gmail.com>
2025-11-22 13:58:49 -0300

    heroku

--- backend-monolito/src/main/kotlin/com/luizgasparetto/backend/monolito/config/cors/CorsConfig.kt
+++ backend-monolito/src/main/kotlin/com/luizgasparetto/backend/monolito/config/cors/CorsConfig.kt
@@ -15,7 +15,9 @@ class CorsConfig {
                 .allowedOriginPatterns(
                     "https://www.agenorgasparetto.com.br",
                     "https://agenorgasparetto.com.br",
-                    "http://localhost:5173"
+                    "http://localhost:5173",
+                    "http://localhost:5174",
+                    "http://localhost:5175"
                 )
                 .allowedMethods("GET")
                 .allowedHeaders("Accept", "Cache-Control", "Content-Type", "Last-Event-ID")
@@ -43,7 +45,9 @@ class CorsConfig {
                 .allowedOriginPatterns(
                     "https://www.agenorgasparetto.com.br",
                     "https://agenorgasparetto.com.br",
-                    "http://localhost:5173"
+                    "http://localhost:5173",
+                    "http://localhost:5174",
+                    "http://localhost:5175"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("Accept", "Content-Type", "Authorization")
